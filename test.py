da743045cafa53183afea4739394570370e3bd02
DANI <dannyandrestecnologia@outlook.com>
2026-09-19 16:55:17 -0500

    Creacion de mejoras a la calculadora

--- test.py
+++ test.py
@@ -3,4 +3,6 @@ from src.main import Calculator
 def test_sum_numbers():
     assert Calculator().sum(2, 2) == 4
 
-#testeando lo de dany
+def test_res_numbers():
+    assert Calculator().res(5, 3) == 2
+
